87b8dc40a85fa11bc21bc3126b37a8f8d6ed855d
David Esposito <david.esposito@bettercloud.com>
2018-12-27 16:41:31 -0500

    Updates

---
+++ precache-manifest.ee9b8c466307a4526c91343d6e5aec25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "297cc4ce1449b7fb4b3e",
+    "url": "/script-eval-poc-micro/static/js/runtime~main.297cc4ce.js"
+  },
+  {
+    "revision": "53f2120ad8b5ab3a04d4",
+    "url": "/script-eval-poc-micro/static/js/main.53f2120a.chunk.js"
+  },
+  {
+    "revision": "10402b1457f6b43b0bb4",
+    "url": "/script-eval-poc-micro/static/js/1.10402b14.chunk.js"
+  },
+  {
+    "revision": "10402b1457f6b43b0bb4",
+    "url": "/script-eval-poc-micro/static/css/1.c2e69a46.chunk.css"
+  },
+  {
+    "revision": "79ee0d895f4ccaf1710026ed6f43d038",
+    "url": "/script-eval-poc-micro/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0fff129d34b1cc02f5594989b69dddeb.js"
+  "/script-eval-poc-micro/precache-manifest.ee9b8c466307a4526c91343d6e5aec25.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/script-eval-poc-micro/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
